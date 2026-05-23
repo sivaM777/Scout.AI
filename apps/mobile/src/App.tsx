77166a8e1d6a15d406341e2d53f572dcc21bebda
sivaM777 <sivamariappan1502@gmail.com>
2026-05-23 16:03:31 +0530

    Wrap app with gesture handler root view

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StatusBar } from "react-native";
 import { AppErrorBoundary } from "./components/AppErrorBoundary";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AppNavigator } from "./navigation/AppNavigator";
 import { AppStateProvider } from "./state/AppState";
@@ -9,12 +10,14 @@ import { theme } from "./theme";
 export default function App() {
   return (
     <AppErrorBoundary>
-      <SafeAreaProvider>
-        <AppStateProvider>
-          <StatusBar barStyle="dark-content" backgroundColor={theme.colors.background} />
-          <AppNavigator />
-        </AppStateProvider>
-      </SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SafeAreaProvider>
+          <AppStateProvider>
+            <StatusBar barStyle="dark-content" backgroundColor={theme.colors.background} />
+            <AppNavigator />
+          </AppStateProvider>
+        </SafeAreaProvider>
+      </GestureHandlerRootView>
     </AppErrorBoundary>
   );
 }
